f445ebf50a21716ec1ac9efffa1f160a5fe303ad
Ian Suvak <ian.suvak@gmail.com>
2023-08-03 11:39:45 -0400

    p2p gater

---
+++ network/p2p/gater.go
@@ -0,0 +1,66 @@
+// Copyright (C) 2019-2023 Algorand, Inc.
+// This file is part of go-algorand
+//
+// go-algorand is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// go-algorand is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with go-algorand.  If not, see <https://www.gnu.org/licenses/>.
+
+package p2p
+
+import (
+	"github.com/libp2p/go-libp2p-core/control"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p/core/peer"
+	ma "github.com/multiformats/go-multiaddr"
+)
+
+// methods defined here implement libp2p's ConnectionGater interface
+
+// InterceptPeerDial implements connmgr.ConnectionGater
+// Trivially allow all outgoing connections right now
+func (_ *Service) InterceptPeerDial(p peer.ID) (allow bool) {
+	return true
+}
+
+// InterceptAddrDial implements connmgr.ConnectionGater
+// Trivially allow all outgoing connections right now
+// TODO: potentially implement preventing re-dialing known bad peers and prevent dialing if we are already at max peers
+func (_ *Service) InterceptAddrDial(peer.ID, ma.Multiaddr) (allow bool) {
+	return true
+
+}
+
+// InterceptAccept tests whether an incipient inbound connection is allowed.
+func InterceptAccept(network.ConnMultiaddrs) (allow bool) {
+
+}
+
+// InterceptSecured tests whether a given connection, now authenticated,
+// is allowed.
+//
+// This is called by the upgrader, after it has performed the security
+// handshake, and before it negotiates the muxer, or by the directly by the
+// transport, at the exact same checkpoint.
+func InterceptSecured(network.Direction, peer.ID, network.ConnMultiaddrs) (allow bool) {
+
+}
+
+// InterceptUpgraded tests whether a fully capable connection is allowed.
+//
+// At this point, the connection a multiplexer has been selected.
+// When rejecting a connection, the gater can return a DisconnectReason.
+// Refer to the godoc on the ConnectionGater type for more information.
+//
+// NOTE: the go-libp2p implementation currently IGNORES the disconnect reason.
+func InterceptUpgraded(network.Conn) (allow bool, reason control.DisconnectReason) {
+
+}
